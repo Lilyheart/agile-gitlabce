600863ab0a1a1e2b06faec3df51f5dc9e4b93dcc
Lily Romano <Lilyheart@users.noreply.github.com>
2019-10-12 21:46:17 -0400

    Refactor js files

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+See Scripts folder for additional Scripts.
+This file contains general code and global variable declarations
+*/
+
 var base_url;
 var gitlab_key;
 var project_id;
@@ -33,118 +38,6 @@ async function update_curr_username() {
       curr_username = data.username
     });
   }
-}
-
-async function get_project_list(url) {
-  console.log(url)
-  project_list = [];
-  url = url + "projects?order_by=name&sort=asc&simple=true&private_token=" + gitlab_key
-
-  // Get number of project pages
-  projectPages = get_header_value(url, "x-total-pages")
-  console.log("Total pages to fetch: " + projectPages)
-
-  // Set up drowndown
-  let dropdown = $('#project-dropdown');
-  dropdown.empty();
-  dropdown.append('<option selected="true" disabled>Choose Project</option>');
-  dropdown.prop('selectedIndex', 0);
-
-  // Fill dropdown
-  console.log("Obtaining data at: " + url + "&page=1")
-  for(i=1; i <= projectPages; i++) {
-    await $.getJSON(url + "&page=" + i, function (data) {
-      // TODO Alphabetize project list THEN make option list
-      project_list = project_list.concat(data)
-      $.each(data, function (key, entry) {
-        let proj_name;
-        if (curr_username == null || curr_username.length == 0) {
-          proj_name = entry.name + " (" + entry.namespace.path + ")";
-        } else {
-          proj_name = entry.name;
-        }
-        dropdown.append($('<option></option>').attr('value', entry.id).text(proj_name));
-      })
-    });
-  }
-
-}
-
-async function getProjects(projFilter) {
-  set_phase("project_start")
-
-  // Get and set variables
-  base_url = document.getElementById("base_url").value;
-  gitlab_key = document.getElementById("gitlab_key").value;
-
-  // Set or clear username
-  await update_curr_username();
-  let url;
-
-  // Build URL and get project list
-  if(projFilter == "all" || curr_username == null) {
-    url = base_url;
-  } else {
-    url = base_url + "users/" + curr_username + "/";
-  }
-
-  await get_project_list(url)
-
-  $('#btnGetIssues').prop('disabled', true);
-  set_phase("project_end")
-}
-
-function enableIssueBtn() {
-  $('#btnGetIssues').prop('disabled', false);
-}
-
-async function get_issues_list(url) {
-  // Get number of project pages
-  projectPages = get_header_value(url, "x-total-pages")
-  console.log("Total pages to fetch: " + projectPages)
-
-  // Get Data
-  issue_list = [];
-  console.log("Obtaining data at: " + url + "&page=1")
-  for(i=1; i <= projectPages; i++) {
-    await $.getJSON(url + "&page=" + i, function(data) {
-      issue_list = issue_list.concat(data)
-    });
-  }
-}
-
-async function getIssues() {
-  set_phase("issue_start")
-
-  // Get and set variables
-  project_id = document.getElementById("project-dropdown").value;
-  let url = base_url + "projects/" + project_id + "/issues?private_token=" + gitlab_key
-
-  // Reset table
-  $("#issuestable").dataTable().fnDestroy()
-  $("#issuestablerows tr").remove();
-
-  await get_issues_list(url);
-
-  $('#issuestable').DataTable({
-    data: issue_list,
-    columns: [
-      { "data": "title"},
-      { "data": "state"},
-      { "data": "time_stats.human_time_estimate"}
-    ],
-    "columnDefs": [{
-      "render": function ( data, type, row ) {
-        return "<a href='" + row.web_url + "' target='_blank'>" + row.title + "</a>"
-      },
-      "targets": 0
-    }]
-  });
-  set_phase("issue_end")
-}
-
-async function testStuff() {
-
 }
 
 function set_phase(new_phase) {
@@ -203,5 +96,4 @@ function set_phase(new_phase) {
 $( document ).ready(function() {
   $("#head").load("resources/header.html");
   set_phase("start");
-  testStuff();
 });
---
+++ scripts/issues.js
@@ -0,0 +1,53 @@
+function enableIssueBtn() {
+  $('#btnGetIssues').prop('disabled', false);
+}
+
+async function load_issue_table(url) {
+  // Reset table
+  $("#issuestable").dataTable().fnDestroy()
+  $("#issuestablerows tr").remove();
+
+  await get_issues_list(url);
+
+  $('#issuestable').DataTable({
+    data: issue_list,
+    columns: [
+      { "data": "title"},
+      { "data": "state"},
+      { "data": "time_stats.human_time_estimate"}
+    ],
+    "columnDefs": [{
+      "render": function ( data, type, row ) {
+        return "<a href='" + row.web_url + "' target='_blank'>" + row.title + "</a>"
+      },
+      "targets": 0
+    }]
+  });
+}
+
+async function get_issues_list(url) {
+  // Get number of project pages
+  projectPages = get_header_value(url, "x-total-pages")
+  console.log("Total pages to fetch: " + projectPages)
+
+  // Get Data
+  issue_list = [];
+  console.log("Obtaining data at: " + url + "&page=1")
+  for(i=1; i <= projectPages; i++) {
+    await $.getJSON(url + "&page=" + i, function(data) {
+      issue_list = issue_list.concat(data)
+    });
+  }
+}
+
+async function getIssues() {
+  set_phase("issue_start")
+
+  // Get and set variables
+  project_id = document.getElementById("project-dropdown").value;
+  let url = base_url + "projects/" + project_id + "/issues?private_token=" + gitlab_key
+
+  await load_issue_table(url)
+
+  set_phase("issue_end")
+}
